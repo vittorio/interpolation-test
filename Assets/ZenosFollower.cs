ec17b792a7886e6e9d9294d81b3f4e27a5d6b2a6
Victor <sexsportandrocknroll@gmail.com>
2020-05-02 16:54:01 +0300

    Zenos paradox example

---
+++ Assets/ZenosFollower.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZenosFollower : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public GameObject poi;
+    public float u = 0.1f;
+    public Vector3 p0, p1, p01;
+
+    private void FixedUpdate() {
+        p0 = this.transform.position;
+        p1 = poi.transform.position;
+        p01 = (1 - u) * p0 + u * p1;
+
+        this.transform.position = p01;
+    }
+}
